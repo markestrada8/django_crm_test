c02878cdea2cac56934dfebac1799460dbf07885
Mark Estrada <mark.estrada.dev>
2023-08-04 13:44:10 -0500

    update data and views

--- db.py
+++ db.py
@@ -1,17 +1,17 @@
 import mysql.connector
 
 database = mysql.connector.connect(
-	host = "localhost",
-	user = "root",
-	password = "",
-    # database = "customers",
-    auth_plugin = "mysql_native_password"
+	host = 'localhost',
+	user = 'root',
+	password = 'mysql',
+    # database = 'customers',
+    auth_plugin = 'mysql_native_password'
 	)
 
 # prepare a cursor object
 cursor_object = database.cursor()
 
 # Create a database
-cursor_object.execute("CREATE DATABASE customers")
+cursor_object.execute('CREATE DATABASE customers')
 
-print("Database created")
+print('Database created')
--- website/views.py
+++ website/views.py
@@ -1,3 +1,55 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+from .forms import SignUpForm
+from .models import Customer
 
-# Create your views here.
+def home(request):
+    customers = Customer.objects.all()
+
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, 'You are logged in')
+            return redirect('home')
+        else:
+            messages.success(request, 'There was an error logging in. Please try again')
+            return redirect('home')
+    else:
+        return render(request, 'home.html', {'customers': customers})
+
+# def login_user(request):
+#     pass
+
+def logout_user(request):
+    logout(request)
+    messages.success(request, 'You have been logged out')
+    return redirect('home')
+
+def register_user(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            messages.success(request, 'You have successfully registered!')
+            return redirect('home')
+    else:
+        form = SignUpForm()
+        return render(request, 'register.html', {'form': form})
+    return render(request, 'register.html', {'form': form})
+
+def customer_item(request, id):
+    if request.user.is_authenticated:
+        customer_item = Customer.objects.get(id=id)
+        return render(request, 'customer.html', {'customer_item': customer_item})
+    else:
+        messages.success(request, 'You must be logged in to view customer information')
+        return redirect('home')
