4e3a9888293f7b86cf86009ac3904679bdb7c806
Mark Estrada <mark.estrada.dev>
2023-08-05 10:27:28 -0500

    update files, add delete and update

--- website/urls.py
+++ website/urls.py
@@ -7,4 +7,7 @@ urlpatterns = [
     path('logout/', views.logout_user, name='logout'),
     path('register/', views.register_user, name='register'),
     path('customer/<int:id>', views.customer_item, name='customer'),
+    path('delete_customer/<int:id>', views.delete_customer, name='delete_customer'),
+    path('add_customer/', views.add_customer, name='add_customer'),
+    path('update_customer/<int:id>', views.update_customer, name='update_customer'),
 ]
--- website/views.py
+++ website/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
-from .forms import SignUpForm
+from .forms import SignUpForm, AddCustomerForm
 from .models import Customer
 
 def home(request):
@@ -14,20 +14,19 @@ def home(request):
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
-            messages.success(request, 'You are logged in')
+            messages.success(request, 'You are logged in.')
             return redirect('home')
         else:
-            messages.success(request, 'There was an error logging in. Please try again')
+            messages.success(request, 'There was an error logging in. Please try again.')
             return redirect('home')
     else:
         return render(request, 'home.html', {'customers': customers})
 
-# def login_user(request):
-#     pass
+# USER CONTROLLERS
 
 def logout_user(request):
     logout(request)
-    messages.success(request, 'You have been logged out')
+    messages.success(request, 'You have been logged out.')
     return redirect('home')
 
 def register_user(request):
@@ -46,10 +45,49 @@ def register_user(request):
         return render(request, 'register.html', {'form': form})
     return render(request, 'register.html', {'form': form})
 
+# CUSTOMER CONTROLLERS
+
 def customer_item(request, id):
     if request.user.is_authenticated:
         customer_item = Customer.objects.get(id=id)
         return render(request, 'customer.html', {'customer_item': customer_item})
     else:
-        messages.success(request, 'You must be logged in to view customer information')
+        messages.success(request, 'You must be logged in to perform that action.')
         return redirect('home')
+
+def delete_customer(request, id):
+    if request.user.is_authenticated:
+        item_to_delete = Customer.objects.get(id=id)
+        item_to_delete.delete()
+        messages.success(request, 'Customer record deleted successfully')
+        return redirect('home')
+    else:
+        messages.success(request, 'You must be logged in to perform that action.')
+        return redirect('home')
+
+def add_customer(request):
+    form = AddCustomerForm(request.POST or None)
+    if request.user.is_authenticated:
+        if request.method == 'POST':
+            if form.is_valid():
+                add_customer = form.save()
+                messages.success(request, 'New customer saved')
+                return redirect('home')
+        return render(request, 'add_customer.html', {'form':form})
+    else:
+        messages.success(request, 'You must be logged in to perform that action.')
+        return redirect('home')
+
+def update_customer(request, id):
+	if request.user.is_authenticated:
+		current_customer = Customer.objects.get(id=id)
+		form = AddCustomerForm(request.POST or None, instance=current_customer)
+		if form.is_valid():
+			form.save()
+			messages.success(request, 'Information has been updated.')
+			return redirect('home')
+		return render(request, 'update_customer.html', {'form':form})
+	else:
+		messages.success(request, 'You must be logged in to perform that action.')
+		return redirect('home')
+
